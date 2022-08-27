7b2328c6fae8acfb4e8750b3a18d47dd96679792
flow2000 <1982989137@qq.com>
2022-08-27 23:44:41 +0800

    增加压缩图片功能

--- random.php
+++ random.php
@@ -6,7 +6,19 @@ $totalFiles = glob($file_dir . "*");
 if ($totalFiles){
     $page = count($totalFiles);
 }
-$json_string = file_get_contents($file_dir.rand(1,$page).".json"); 
+$start_page=1;
+$cover=$_GET['cover'];
+// 是否请求封面图片
+if ($cover!='true'){
+    $start_page = 236; // 236到最新页都是1920x1080
+}
+$json_string = file_get_contents($file_dir.rand($start_page,$page).".json"); 
 $data = json_decode($json_string,true);
-$imgurl = $data["data"]["data"][rand(0,9)]["url"];
+$imgurl = "";
+if ($cover=='true'){
+    $imgurl="https://cdn.panghai.top".$data["data"]["data"][rand(0,9)]["path"];
+    $imgurl = str_replace("top/bing","top/compress-bing",$imgurl);
+}else{
+    $imgurl = $data["data"]["data"][rand(0,9)]["url"];
+}
 header("Location: $imgurl");
