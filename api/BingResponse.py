337d50ea6531c4846420bdd046e4ea4f1c4d62de
pgh1038 <1982989137@qq.com>
2026-01-01 21:55:38 +0800

    feat:增加返回的格式

--- api/BingResponse.py
+++ api/BingResponse.py
@@ -6,6 +6,16 @@ def success(msg="操作成功",data=None):
         res_json['data']=data
     return res_json
 
+def table_success(msg="操作成功",data=None,total=None):
+    res_json = {}
+    res_json['code']=200
+    res_json['msg']=msg
+    if data!=None:
+        res_json['data']=data
+    if total!=None:
+        res_json['total']=total
+    return res_json
+
 def error(msg="操作失败",data=None):
     res_json = {}
     res_json['code']=500
--- api/mongodbapi.py
+++ api/mongodbapi.py
@@ -43,7 +43,7 @@ def query_all(page,limit,order,w,h,uhd,mkt,year=None):
         item['url']=item['url'].replace(util.contact_w_h(settings.DEFAULT_W,settings.DEFAULT_H),link_str)
         data.append(item)
     total=len(data)
-    return BingResponse.success(data=data,total=total)
+    return BingResponse.table_success(data=data,total=total)
 
 def query_total_num(mkt):
     return BingResponse.success(data=get_count(mkt))
